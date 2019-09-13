79cd1e7d14bb10e04a2e1b0c6b10ce128a141aec
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-09-13 15:21:54 -0400

    Adds gitignore file

---
+++ Resturant-Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resturant-Tinder
+//
+//  Created by Mr Wonderful on 9/13/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
