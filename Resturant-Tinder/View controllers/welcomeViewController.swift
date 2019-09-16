396d047ae32bbee8384e333cb847861f104afc11
Jason Ruan <jasonruan@pursuit.org>
2019-09-16 15:09:58 -0400

    Organizes welcomeViewController into sections

--- Resturant-Tinder/View controllers/welcomeViewController.swift
+++ Resturant-Tinder/View controllers/welcomeViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-//dont touch
+    //dont touch
     @IBOutlet var selectCatergoryButtonView: UIView!
     //dont touch
     @IBOutlet var randomButtonView: UIView!
     
-    
+    //MARK: - IBOutlets
     @IBOutlet var randomButton: UIButton!
     @IBOutlet var selectCatergoryButton: UIButton!
     @IBOutlet var welcomeLabel: UILabel!
+    
+    //MARK: - Properties
     var setUserName:String = ""
+    
+    //MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
        welcomeLabel.text = setUserName
