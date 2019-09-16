f7d776c1bfa5c07f8a5aebebbe660d7e7397f037
Tia Lendor <TiaLendon@pursuit.org>
2019-09-16 16:51:57 -0400

    APIKeys file created

---
+++ Resturant-Tinder/APIClients/TeamAPIKeys.swift
@@ -0,0 +1,17 @@
+//
+//  TeamAPIKeys.swift
+//  Resturant-Tinder
+//
+//  Created by Tia Lendor on 9/16/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import Foundation
+
+class TeamAPIKeys {
+    
+    var tiaAPIKey = "WA9Airc5j-ySXtPyuRJ3sKactT0uw3f6rz81Q4AMX7SEbyaj7Pi1g4TKM0MbvCZ-JCJAGb7BeWRh0kNNNJ2YNf7CI3fAgr8GFiTiLDFCS5XLIoupMzUdCDaRuhl8XXYx"
+    var tiaClientID = "NCnfNOhKwLcsj-fIfTY4FA"
+    
+}
+
