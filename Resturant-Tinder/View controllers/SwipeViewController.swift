d84d8b020ec874d1cf02cb7c38d0ba6f2f9cc0e8
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.home>
2019-09-15 21:29:57 -0400

    Adds buttons to the menu view with animation when slideViewMenuButton is pressed

--- Resturant-Tinder/View controllers/SwipeViewController.swift
+++ Resturant-Tinder/View controllers/SwipeViewController.swift
@@ -9,12 +9,27 @@ class SwipeViewController: UIViewController {
     
     @IBOutlet var shadowView: UIView!
     @IBOutlet var menuButton: UIBarButtonItem!
-    @IBOutlet var slideViewMenuButton: UIButton!
+    @IBOutlet var slideViewMenuButtons: UIButton!
+    
+    @IBOutlet var homeButton: UIButton!
+    @IBOutlet var addAllButton: UIButton!
+    
+    @IBOutlet var reloadButton: UIButton!
+    @IBOutlet var clearAll: UIButton!
     var divisorNumber:CGFloat!
     
-    @IBOutlet var shadowViewTrsilingContraints: NSLayoutConstraint!
+    
+    
+    
+    @IBOutlet var homeButtonTopConstraints: NSLayoutConstraint!
+    @IBOutlet var addAllButtonTopConstraints: NSLayoutConstraint!
+    @IBOutlet var clearAllButtonTopConstraints: NSLayoutConstraint!
+    @IBOutlet var reloadButtonTopConstraints: NSLayoutConstraint!
+    
+    @IBOutlet var shadowViewTrailingContraints: NSLayoutConstraint!
     @IBOutlet var menubarLeadindContraits: NSLayoutConstraint!
     var menuDisplayed = false
+    var slideMenuDisplayed = false
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,7 +46,24 @@ class SwipeViewController: UIViewController {
         shadowView.layer.shadowOpacity = 1
         shadowView.layer.shadowRadius = 5
         
+        sliderMenuButtonInitialState()
+    }
+    
+    func sliderMenuButtonInitialState(){
+        homeButtonTopConstraints.constant = -40
+        addAllButtonTopConstraints.constant = -40
+        clearAllButtonTopConstraints.constant = -40
+        reloadButtonTopConstraints.constant = -40
         
+        homeButton.alpha = 0
+        addAllButton.alpha = 0
+        clearAll.alpha = 0
+        reloadButton.alpha = 0
+        
+        homeButton.tintColor = .white
+        addAllButton.tintColor = .white
+        clearAll.tintColor = .white
+        reloadButton.tintColor = .white
     }
     func printFunction(){
         print("main view \(view.center.x)")
@@ -105,14 +137,14 @@ class SwipeViewController: UIViewController {
         }
     }
     
-    @IBAction func menuButtonPressed(_ sender: UIBarButtonItem) {
+    @IBAction func navigationMenuButtonPressed(_ sender: UIBarButtonItem) {
         
         switch menuDisplayed{
         case false:
             // animates the menu view
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.menubarLeadindContraits.constant = -20
-                self.slideViewMenuButton.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+                
                 self.view.layoutIfNeeded()
                 
                 self.menuDisplayed = true
@@ -122,7 +154,7 @@ class SwipeViewController: UIViewController {
             
             // animates the shadow view
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-                self.shadowViewTrsilingContraints.constant = 0
+                self.shadowViewTrailingContraints.constant = 0
                 self.view.layoutIfNeeded()
             }, completion: nil)
             
@@ -132,8 +164,8 @@ class SwipeViewController: UIViewController {
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.menubarLeadindContraits.constant = -250
                 self.view.layoutIfNeeded()
-                self.slideViewMenuButton.transform = CGAffineTransform.identity
-
+                self.slideViewMenuButtons.transform = CGAffineTransform.identity
+                
                 self.menuDisplayed = false
             }) { (true) in
                 self.menuButton.tintColor = .black
@@ -141,10 +173,47 @@ class SwipeViewController: UIViewController {
             
             
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-                self.shadowViewTrsilingContraints.constant = -414
+                self.shadowViewTrailingContraints.constant = -414
                 self.view.layoutIfNeeded()
             }, completion: nil)
             
         }
     }
+    
+    @IBAction func slidweMenuButtonPressed(_ sender: UIButton) {
+        
+        switch slideMenuDisplayed{
+        case false:
+            setupliderMenuButtons(Value: 120, bool: true, alpha: 1, Tranform: .init(rotationAngle: (CGFloat(Double.pi / 2))), color: .blue)
+      slideViewMenuButtons.tintColor = .red
+        case true:
+            setupliderMenuButtons(Value: -40, bool: false, alpha: 0, Tranform: .identity, color: .white)
+     slideViewMenuButtons.tintColor = .black
+        }
+        
+        
+        
+    }
+    func setupliderMenuButtons(Value:CGFloat, bool:Bool, alpha:CGFloat, Tranform: CGAffineTransform, color:UIColor){
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.slideViewMenuButtons.transform = Tranform
+            self.homeButtonTopConstraints.constant = Value
+            self.addAllButtonTopConstraints.constant = Value
+            self.clearAllButtonTopConstraints.constant = Value
+            self.reloadButtonTopConstraints.constant = Value
+            
+            self.homeButton.alpha = alpha
+            self.addAllButton.alpha = alpha
+            self.clearAll.alpha = alpha
+            self.reloadButton.alpha = alpha
+            self.view.layoutIfNeeded()
+        }, completion: { (true ) in
+            self.homeButton.tintColor = color
+            self.addAllButton.tintColor = color
+            self.clearAll.tintColor = color
+            self.reloadButton.tintColor = color
+        })
+        slideMenuDisplayed = bool
+    }
+    
 }
